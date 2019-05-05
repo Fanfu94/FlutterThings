d8e04b8fd2b11866970eb37d0a556b626d9c5dea
Fabrizio Siciliano <sicilianofabrizio@gmail.com>
2019-05-05 14:53:36 +0200

    [First release]

---
+++ bemis_app/lib/FeatureHandler.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FeatureHandler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+  bool show = false;
+  void _toggleFeatures(){
+    show = !show;
+  }
+}
